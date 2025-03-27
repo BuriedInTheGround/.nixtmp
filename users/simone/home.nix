d3e32f7205a863d22a9ef6a2cd8f75792ea61210
Simone Ragusa <hi@interrato.dev>
2025-03-27 15:46:39 +0100

    simone: increase home-manager state version to 24.11

--- users/simone/home.nix
+++ users/simone/home.nix
@@ -1720,5 +1720,5 @@ in {
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "23.05";
+  home.stateVersion = "24.11";
 }
